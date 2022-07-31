87fd6d9411dd775e40cd8653f79a8c09bc54a201
Nattaparin <nattaparin455@gmail.com>
2022-07-31 20:28:02 +0700

    3.2

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,7 +1,11 @@
 <template>
   <h1>Events For Good</h1>
   <div class="events">
-    <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <EventCard
+      v-for="event in events"
+      :key="event.id"
+      :event="event"
+    ></EventCard>
     <div class="pagination">
       <router-link
         id="page-prev"
